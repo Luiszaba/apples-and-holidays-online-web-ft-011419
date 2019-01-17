d4d9889d555cd382c42b7291ac962e901105c64a
Luis Zabala <luis.zabala076@gmail.com>
2019-01-17 17:25:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ end
 
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
-    season.map do |holiday, supply|
+    season.each do |holiday, supply|
     holiday if supply.include?("BBQ")
 end
 end
